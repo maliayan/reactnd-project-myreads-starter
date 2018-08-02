a7a2047b5625e573b5c14fbb19ba5e67676bc9d7
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-08-03 00:23:21 +0300

    ListBooks listing functionality added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    // showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <ListBooks
+            books={this.state.books}
+          />
         )} />
         <Route path='/search' render={() => (
           <SearchBooks />
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -16,12 +16,15 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
-                  <li>
-                    <Book />
-                  </li>
+                  {
+                    this.props.books
+                      .filter(book => book.shelf === 'currentlyReading')
+                      .map(book => (
+                        <li key={book.id}>
+                          <Book/>
+                        </li>
+                      ))
+                  }
                 </ol>
               </div>
             </div>
@@ -29,12 +32,15 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
-                  <li>
-                    <Book />
-                  </li>
+                {
+                  this.props.books
+                    .filter(book => book.shelf === 'wantToRead')
+                    .map(book => (
+                      <li key={book.id}>
+                        <Book/>
+                      </li>
+                    ))
+                }
                 </ol>
               </div>
             </div>
@@ -42,15 +48,15 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <Book />
-                  </li>
-                  <li>
-                    <Book />
-                  </li>
-                  <li>
-                    <Book />
-                  </li>
+                {
+                  this.props.books
+                    .filter(book => book.shelf === 'read')
+                    .map(book => (
+                      <li key={book.id}>
+                        <Book/>
+                      </li>
+                    ))
+                }
                 </ol>
               </div>
             </div>
