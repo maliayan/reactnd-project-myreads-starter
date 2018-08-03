67729c7b7b13996c4f561a6f07ea269d0cca386f
Mehmet Ali Ayan <maliayan34@gmail.com>
2018-08-03 08:00:49 +0300

    Beautified Book.js

--- src/Book.js
+++ src/Book.js
@@ -47,7 +47,14 @@ class Book extends Component {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${imageLinks.thumbnail}")` }}></div>
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${imageLinks.thumbnail}")`
+            }}>
+          </div>
           <div className="book-shelf-changer"></div>
           <Select
               value={selectedOption}
